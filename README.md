1c0d3c3808b40ec821e4792a94e40307219bf2ab
ddxgz <ddxgz19880813@gmail.com>
2020-11-13 10:15:07 +0100

    Add vscode-projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # url-opener
 A simple extension for Albert launcher to open predefined URLs
+# vscode-projects
+An extension for Albert launcher to open VS Code projects quickly
---
+++ vscode-projects/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""List and open your Atom projects.
+Synopsis: <trigger> [filter]"""
+
+import os
+import re
+from pathlib import Path
+
+from albertv0 import *
+
+__iid__ = "PythonInterface/v0.1"
+__prettyname__ = "VS Code Projects"
+__version__ = "1.0"
+__trigger__ = "code "
+__author__ = "Cong Peng"
+
+projects_file = str(Path.home()) + "/code"
+projects = []
+
+
+def handleQuery(query):
+    if not query.isTriggered:
+        return
+
+    projects = os.listdir(projects_file)
+    info(projects)
+
+    stripped = query.string.strip()
+
+    items = []
+    for project in projects:
+        if re.search(stripped, project, re.IGNORECASE):
+            items.append(Item(id=__prettyname__,# + project,
+                              # icon=iconPath,
+                              text=project,
+                              subtext="Group",
+                              completion=query.rawString,
+                              actions=[
+                                  ProcAction(text="Open project in VS Code",
+                                             commandline=["code", project],
+                                             cwd=projects_file)
+                              ]))
+    return items
